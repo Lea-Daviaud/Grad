e19ab04efc5bd41121492b9b572acef71f67a81f
leadaviaud <lea.daviaud@lycee-elie-vinet.org>
2025-05-10 10:35:31 +0200

    fini fini

--- message.php
+++ message.php
@@ -5,6 +5,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>GRAD</title>
+         <link rel="stylesheet" href="./CSSAccueil.css">
+    <link rel="stylesheet" href="./CSSEntete.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
 
@@ -13,6 +20,8 @@
         <br>
 
         <?php
+        include "entete.php";
+
         $nom = $_POST["name"];
         $surname = $_POST["surname"];
         $carctspe = "#^[a-z0-9]+$#i";
@@ -56,9 +65,10 @@ $stmt->execute([
 
         
         };
-    
+    include "piedpage.php";
     ?>
         <br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     </body>
 </html>
--- panier.php
+++ panier.php
@@ -79,5 +79,7 @@ function totalPanier($produits, $panier) {
     <?php endif; ?>
 </div>
  <?php include "piedpage.php"; ?>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
